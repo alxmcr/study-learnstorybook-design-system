4a43835a0718ff82eee8fdafc3573a6d85b2c0f5
Alejandro M. Coca <alxmcr@outlook.com>
2021-06-14 12:43:43 -0400

    Return original behavior

--- src/Button.js
+++ src/Button.js
@@ -33,8 +33,7 @@ const SIZES = {
 };
 
 const StyledButton = styled.button`
-  border: 10px solid red;
-  font-size: 20px;
+  border: 0;
   border-radius: 3em;
   cursor: pointer;
   display: inline-block;
